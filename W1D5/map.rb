9c0b9706c7243cf8787d1e33632a3101a4716618
Daniel <d.chen323@gmail.com>
2018-04-19 19:03:06 -0700

    Removed use of hash in map class and only to use arrays

--- W1D5/map.rb
+++ W1D5/map.rb
@@ -1,24 +1,29 @@
 class Map
 
-  def initialize(array_map = Hash.new{|h,k| h[k] = 0})
+  attr_accessor :array_map
+
+  def initialize(array_map = [])
     @array_map = array_map
   end
 
   def assign(key,value)
-    @array_map[key] = value
+    key_made = @array_map.index {|values| values.first == key}
+    key_made ? @array_map[key_made][1] = value : @array_map << [key,value]
   end
 
   def lookup(key)
-    @array_map.select do |array|
-      array.first == key
+    @array_map.each do |pairs|
+      return pairs.last if pairs.first == key
     end
   end
 
   def remove(key)
-    @array_map.delete_if{|k,_| k==key}
+    @array_map.each do |pairs|
+      @array_map.delete(pairs) if pairs.first == key
+    end
   end
 
   def show
-    @array_map.to_a.dup
+    @array_map.dup
   end
 end
